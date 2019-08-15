7342a3e48f5094b684111f1c514c3a5b6e317cc4
RobertClayton <robbie.c.clayton@gmail.com>
2019-08-15 23:05:33 +0100

    building the view

---
+++ src/BookStorePage.kt
@@ -0,0 +1,7 @@
+class BookStorePage(private val presenter: BookStoreContract.Presenter) : BookStoreContract.View {
+    override fun showBooks(books: List<Book>) {}
+
+    override fun showLoader() {}
+
+    override fun hideLoader() {}
+}
