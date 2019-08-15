51570585a0a0b28c4fae162e5c855f8df38dc345
RobertClayton <robbie.c.clayton@gmail.com>
2019-08-15 23:14:04 +0100

    Fetching a list of books from a URL

--- src/BookStorePresenter.kt
+++ src/BookStorePresenter.kt
@@ -1,3 +1,5 @@
+import org.w3c.xhr.XMLHttpRequest
+
 class BookStorePresenter : BookStoreContract.Presenter {
     private lateinit var view: BookStoreContract.View
 
@@ -5,5 +7,24 @@ class BookStorePresenter : BookStoreContract.Presenter {
         this.view = view
     }
 
-    override fun loadBooks() {}
+    override fun loadBooks() {
+        view.showLoader()
+        getAsync(API_URL) { response ->
+            val books = JSON.parse<Array<Book>>(response)
+
+            view.hideLoader()
+            view.showBooks(books.toList())
+        }
+    }
+
+    private fun getAsync(url: String, callback: (String) -> Unit) {
+        val xmlHttp = XMLHttpRequest()
+        xmlHttp.open("GET", url)
+        xmlHttp.onload = {
+            if (xmlHttp.readyState == 4.toShort() && xmlHttp.status == 200.toShort()) {
+                callback.invoke(xmlHttp.responseText)
+            }
+        }
+        xmlHttp.send()
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
-
+    val bookStorePresenter = BookStorePresenter()
+    val bookStorePage = BookStorePage(bookStorePresenter)
+    bookStorePresenter.attach(bookStorePage)
+    bookStorePresenter.loadBooks()
 }
+
+val API_URL = js("getApiUrl()") as String
