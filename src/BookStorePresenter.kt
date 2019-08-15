a58696b4bfea878e47fea1ded59a5a1bef3cfdfd
RobertClayton <robbie.c.clayton@gmail.com>
2019-08-15 22:45:04 +0100

    building the Presenter

---
+++ src/BookStorePresenter.kt
@@ -0,0 +1,9 @@
+class BookStorePresenter : BookStoreContract.Presenter {
+    private lateinit var view: BookStoreContract.View
+
+    override fun attach(view: BookStoreContract.View) {
+        this.view = view
+    }
+
+    override fun loadBooks() {}
+}
