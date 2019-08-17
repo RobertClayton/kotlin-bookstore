84ec0db54ee8d9bbeeccc53a3dac1998ee69d8d9
RobertClayton <robbie.c.clayton@gmail.com>
2019-08-17 23:01:42 +0100

    linking the loader

--- src/BookStorePage.kt
+++ src/BookStorePage.kt
@@ -1,7 +1,17 @@
+import kotlin.browser.document
+import org.w3c.dom.HTMLDivElement
+
 class BookStorePage(private val presenter: BookStoreContract.Presenter) : BookStoreContract.View {
+    private val loader = document.getElementById("loader") as HTMLDivElement
+    private val content = document.getElementById("content") as HTMLDivElement
+
     override fun showBooks(books: List<Book>) {}
 
-    override fun showLoader() {}
+    override fun showLoader() {
+        loader.style.visibility = "visible"
+    }
 
-    override fun hideLoader() {}
+    override fun hideLoader() {
+        loader.style.visibility = "hidden"
+    }
 }
