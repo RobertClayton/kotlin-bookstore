6513302abdb1ddab6a89b63c82fd171e6552191e
RobertClayton <robbie.c.clayton@gmail.com>
2019-08-17 23:56:59 +0100

    now displaying all books

--- src/BookStorePage.kt
+++ src/BookStorePage.kt
@@ -4,8 +4,19 @@ import org.w3c.dom.HTMLDivElement
 class BookStorePage(private val presenter: BookStoreContract.Presenter) : BookStoreContract.View {
     private val loader = document.getElementById("loader") as HTMLDivElement
     private val content = document.getElementById("content") as HTMLDivElement
+    private val cardBuilder = CardBuilder()
 
-    override fun showBooks(books: List<Book>) {}
+    override fun showBooks(books: List<Book>) {
+        books.forEach { book ->
+            val card = cardBuilder.build(book)
+            content.appendChild(card)
+        }
+    }
+
+    fun show() {
+        presenter.attach(this)
+        presenter.loadBooks()
+    }
 
     override fun showLoader() {
         loader.style.visibility = "visible"
--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,7 @@
 fun main(args: Array<String>) {
     val bookStorePresenter = BookStorePresenter()
     val bookStorePage = BookStorePage(bookStorePresenter)
-    bookStorePresenter.attach(bookStorePage)
-    bookStorePresenter.loadBooks()
+    bookStorePage.show()
 }
 
 val API_URL = js("getApiUrl()") as String
